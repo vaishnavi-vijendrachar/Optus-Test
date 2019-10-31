18ae919eac14e115471466d3f7744dd1d9b01aac
vaishnavi-vijendrachar <vaishnavi.vijendrachar@gmail.com>
2019-10-31 23:33:54 +1100

    adding photo module

---
+++ app/src/main/java/com/vaishnavi/optustest/PhotoActivity.kt
@@ -0,0 +1,37 @@
+package com.vaishnavi.optustest
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+
+class PhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photos)
+        /*setSupportActionBar(toolbar)
+        toolbar.setTitle("Photos")*/
+
+        //get data from intent
+        var intent : Intent =  getIntent()
+        var url = intent.getStringExtra("url")
+        var intentTitle = intent.getStringExtra("title")
+
+        var photo = findViewById<ImageView>(R.id.photo)
+        var title = findViewById<TextView>(R.id.title)
+
+
+            Glide.with(applicationContext)
+                .load(url)
+                .centerCrop()
+                .skipMemoryCache(false)
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                .into(photo)
+
+
+        title.setText(intentTitle)
+    }
+}
